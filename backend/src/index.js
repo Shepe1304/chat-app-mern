44b054ba1eb4b47ae193a9f17f9d298679c9267c
Shepe1304 <hqgiang1346@gmail.com>
2025-04-04 00:09:36 -0400

    Signup function in backend with jwt token generation and password hashing using bcrypt

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,20 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
-app.use("/api/auth", authRoutes);
-
 const PORT = process.env.PORT;
+
+app.use(express.json()); // Middleware that allows you to extract json data out of the body
+
+app.use("/api/auth", authRoutes); // must be below middleware
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
-  connectDB()
+  connectDB();
 });
